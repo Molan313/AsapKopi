a5f056ec834fbb6e2f4630d54dd4fa40424699cf
Molan313 <faisalmasrimaulana@gmail.com>
2024-06-19 00:56:07 +0700

    menambahkan produk

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 document.querySelector('#hamburger-menu').onclick = () => {
-    event.preventDefault()
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
 // Klik di luar sidebar untuk menghilangkan nav
@@ -15,4 +15,29 @@ document.addEventListener('click', function (e) {
     }
 });
 
-// 1:37:00
+// toggle class active search form
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector("#search-button").onclick = () => {  
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// klik di luar
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector("#search-button");
+
+document.addEventListener('click', function (e) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+});
+
+// 45:10
